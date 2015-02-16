db78427f36bc92cffb12c25ceba893a7aa9081e3
Lus Dorelli de Abreu <fushigishi@gmail.com>
2015-02-16 20:34:28 -0200

    2 pombo

---
+++ Problemas/Dois_Pombinhos/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void) {
+	long long x;
+	while (cin >> x) {
+		if (x%3 == 1) cout << "KWOOO" << endl;
+		else cout << "KWEE" << endl;
+	}
+}
---
+++ Problemas/Dois_Pombinhos/gen.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main (void) {
+	for (int i = 0; i < 1000; ++i) cout << rand() << endl;
+}
